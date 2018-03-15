c49144e72382ce993fc5d5332284402600692117
Inguarus <35971740+Inguarus@users.noreply.github.com>
2018-03-15 02:19:52 +0200

    Method parameter

---
+++ Test.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+import static testpackage.SortingArray.sortingArray;
+
+public class Test {
+    public static void main(String[] args) {
+
+        int arr[] = new int[15];
+        sortingArray(arr);
+    }
+
+}
+
+
